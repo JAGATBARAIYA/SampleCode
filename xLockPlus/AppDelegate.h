26a39abf38f4ccfe441f8ab7373654207a60b9fb
Manish <E2M-MINI@Manishs-Mac-mini-2.local>
2014-12-05 12:09:01 +0530

    Initial Commit

---
+++ xLockPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xLockPlus
+//
+//  Created by Manish on 05/12/14.
+//  Copyright (c) 2014 E2M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xLockPlus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  xLockPlus
+//
+//  Created by Manish on 05/12/14.
+//  Copyright (c) 2014 E2M. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
